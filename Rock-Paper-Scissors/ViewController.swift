be54057824e0bab537ce75b0e1ed3658525a9959
Pavel.818 <Pavel.818@yandex.ru>
2023-09-23 00:05:01 -0700

    Initial Commit

---
+++ Rock-Paper-Scissors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rock-Paper-Scissors
+//
+//  Created by Pavel on 2023-09-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
