8c33cc8443711ceaa933c82753c934dd9a6a8608
Pavel.818 <Pavel.818@yandex.ru>
2023-09-24 00:12:20 -0700

    addElements

--- Rock-Paper-Scissors/UIKit/UIView + Extensions.swift
+++ Rock-Paper-Scissors/UIKit/UIView + Extensions.swift
@@ -5,4 +5,14 @@
 //  Created by Pavel on 2023-09-23.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    func addShadowOnView() {
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowOffset = CGSize(width: 0.0, height: 3.0)
+        layer.shadowOpacity = 0.7
+        layer.shadowRadius = 1
+    }
+}
